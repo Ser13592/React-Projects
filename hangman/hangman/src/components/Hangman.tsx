9ebeea8c6b77feea17e46a60ea857001c10d0cbd
Ser13592 <clearsky13592@gmail.com>
2023-01-21 23:31:01 +0000

    Changing hang man position

--- hangman/hangman/src/components/Hangman.tsx
+++ hangman/hangman/src/components/Hangman.tsx
@@ -1,25 +1,25 @@
 const HEAD=(
-    <div style={{width:'50px',height:'50px',border:'10px solid black', borderRadius:'100%', position:'absolute',top:'50px',right:'-185px',marginBottom:'2rem' }}></div>
+    <div style={{width:'50px',height:'50px',border:'10px solid black', borderRadius:'100%', position:'absolute',top:'50px',right:'-85px',marginBottom:'2rem' }}></div>
 )
 
 const BODY=(
-    <div style={{width:'10px', height:'100px',  border:'1px solid black', background:'black', position:'absolute',top:'110px',right:'-157px' }}></div>
+    <div style={{width:'10px', height:'100px',  border:'1px solid black', background:'black', position:'absolute',top:'110px',right:'-57px' }}></div>
 )
 
 const LEFT_ARM=(
-    <div style={{width:'100px', height:'10px',  border:'1px solid black', background:'black', position:'absolute',top:'130px',right:'-150px' }}></div>
+    <div style={{width:'100px', height:'10px',  border:'1px solid black', background:'black', position:'absolute',top:'130px',right:'-50px' }}></div>
 )
 
 const RIGHT_ARM=(
-    <div style={{width:'100px', height:'10px',  border:'1px solid black', background:'black', position:'absolute',top:'130px',right:'-255px' }}></div>
+    <div style={{width:'100px', height:'10px',  border:'1px solid black', background:'black', position:'absolute',top:'130px',right:'-155px' }}></div>
 )
 
 const LEFT_LEG=(
-    <div style={{width:'100px', height:'10px',  border:'1px solid black', background:'black', position:'absolute',top:'200px',right:'-155px', transformOrigin:'right bottom', transform:'rotate(-45deg)' }}></div>
+    <div style={{width:'100px', height:'10px',  border:'1px solid black', background:'black', position:'absolute',top:'200px',right:'-55px', transformOrigin:'right bottom', transform:'rotate(-45deg)' }}></div>
 )
 
 const RIGHT_LEG=(
-    <div style={{width:'100px', height:'10px',  border:'1px solid black', background:'black', position:'absolute',top:'200px',right:'-250px', transformOrigin:'left bottom', transform:'rotate(45deg)' }}></div>
+    <div style={{width:'100px', height:'10px',  border:'1px solid black', background:'black', position:'absolute',top:'200px',right:'-150px', transformOrigin:'left bottom', transform:'rotate(45deg)' }}></div>
 )
 const BODY_PARTS=[HEAD,BODY, LEFT_ARM, RIGHT_ARM, LEFT_LEG, RIGHT_LEG];
 
@@ -30,8 +30,8 @@ type HangmanProps={
 export function HangMan( {numberOfGuesses}:HangmanProps){
 return(
 <div style={{width:'300px', position:'relative'}}>
-    <div style={{width:'10px', height:'50px',  border:'1px solid black', background:'black', position:'absolute', top:'0px',right:'-155px'}}></div>
-    <div style={{height:'10px',width:'300px', border:'1px solid black', background:'black',marginLeft:'145px'}}></div>
+    <div style={{width:'10px', height:'50px',  border:'1px solid black', background:'black', position:'absolute', top:'0px',right:'-55px'}}></div>
+    <div style={{height:'10px',width:'200px', border:'1px solid black', background:'black',marginLeft:'145px'}}></div>
     <div style={{width:'10px', height:'300px',  border:'1px solid black', background:'black', marginLeft:'145px'}}></div>
     <div style={{height:'10px', border:'1px solid black', background:'black'}}></div>
     {BODY_PARTS.slice(0,numberOfGuesses)}
