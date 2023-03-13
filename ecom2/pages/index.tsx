e8bcc66ce8aaec8c20740e969aef2dba57a64468
Ser13592 <clearsky13592@gmail.com>
2023-03-13 01:43:22 +0000

    updated

--- ecom2/pages/index.tsx
+++ ecom2/pages/index.tsx
@@ -5,16 +5,43 @@ import {client} from '../lib/client'
 
 const inter = Inter({ subsets: ['latin'] })
 
+interface Product {
+  slug: string;
+  // add more properties as needed
+}
+
+interface Person {
+  slug: string;
+  name: string;
+  family:string;
+  email:string
+  // add more properties as needed
+}
+
 interface HomeProps<T> {
-  products: T[];
+  products: Product[];
+  people: Person[];
 }
 
 
-export default function Home<T>({products}:HomeProps<T>) {
+export default function Home<T>({products,people}:HomeProps<T>) {
   console.log(products)
   return (
     <>
-      hi
+      {people.map(p=>(
+        <div style={{  display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fill, minmax(150px, 1fr))', 
+        alignItems: 'center', // vertically center span elements
+        color: 'blue',
+        border: '1px solid black',
+            padding: '10px'
+            }} key={p.slug}>
+          <span>{p.name}</span>
+          <span>{p.family}</span>
+          <span>{p.email}</span>
+         </div>
+        
+        ))}
     </>
   )
   
@@ -24,9 +51,11 @@ export const getServerSideProps = async () => {
   const query = '*[_type == "product"]';
   const products = await client.fetch(query);
 
+  const query2= '*[_type == "people"]';
+  const people = await client.fetch(query2);
 
 
   return {
-    props: { products }
+    props: { products,people }
   }
 }
